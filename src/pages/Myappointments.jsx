a75543e2aa6a56384574062f8c74e7efb9a703f3
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-06-14 17:50:29 +0530

    add functionality

--- src/pages/Myappointments.jsx
+++ src/pages/Myappointments.jsx
@@ -129,8 +129,8 @@ export default function Myappointments() {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              {!item.cancelled && item.payment && <button className='sm:min w-48 border rounded text-stone-500 bg-indigo-500'> paid</button>}
-              {!item.cancelled && <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-200 rounded hover:bg-blue-500 hover:text-white transition-all duration-300 cursor-pointer'>Pay Online</button>}
+              {!item.cancelled && item.payment && <button className='sm:min w-48 py-2 border rounded text-stone-500 bg-indigo-50'> paid</button>}
+              {!item.cancelled && !item.payment &&<button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-200 rounded hover:bg-blue-500 hover:text-white transition-all duration-300 cursor-pointer'>Pay Online</button>}
               {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border border-gray-200 rounded hover:bg-red-600 hover:text-white transition-all duration-300 cursor-pointer'>Cancel appointment</button>}
               {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
             </div>
